85eaf5b6bd2f50ed806b2648d4fb8289fb90714f
William Ma <12377941+williamma12@users.noreply.github.com>
2021-03-15 12:18:56 -0500

    Set up CI
    Signed-off-by: William Ma <12377941+williamma12@users.noreply.github.com>

---
+++ configure.sh
@@ -0,0 +1,9 @@
+# Produce files Make and Makefile
+
+cp -f Make.in Make
+
+COQ_VERSION_LINE=$(coqc --version | head -1)
+COQ_VERSION=`echo $COQ_VERSION_LINE | sed 's/The Coq Proof Assistant, version 8\.\([^ +]*\).*/\1/'`
+DIRECTORIES="theories"
+
+${COQBIN}coq_makefile -f Make -o Makefile
